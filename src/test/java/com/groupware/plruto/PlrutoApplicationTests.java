66597fdda43c0f1d716e22a5ae3c2eb55ce117b6
rewq985 <bnbt3@naver.com>
2022-01-08 11:22:11 +0900

    플루토 프로젝트 시작

---
+++ src/test/java/com/groupware/plruto/PlrutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupware.plruto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlrutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
