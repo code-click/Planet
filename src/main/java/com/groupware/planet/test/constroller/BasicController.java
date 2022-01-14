21fd1f5a5eb34e68b66846007920b13aae61c4ad
rewq985 <bnbt3@naver.com>
2022-01-14 20:20:14 +0900

    부트스트랩

--- src/main/java/com/groupware/planet/test/constroller/BasicController.java
+++ src/main/java/com/groupware/planet/test/constroller/BasicController.java
@@ -5,13 +5,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/test")
 public class BasicController {
     private final TestService testService;
 
-    @GetMapping("/")
+    @GetMapping("/test")
     public String test(Model model){
         model.addAttribute("test", testService.test());
         return "index";
